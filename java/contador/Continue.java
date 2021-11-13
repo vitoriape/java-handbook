6497cfd63bf8a03436fe607ed0e022da3977865a
Vitória Peçanha <vitoriapecanha.log@gmail.com>
2021-11-13 14:50:53 -0300

    Update Continue.java

--- java/contador/Continue.java
+++ java/contador/Continue.java
@@ -1,18 +1,19 @@
 class Continue {
-  public static void main(String[] args) {
+  public static void main( String[] args ) {
     int contador;
-    for(contador = 0; contador <10; contador ++)
-    {
+    for( contador = 0 ; contador < 10 ; contador++ ) {
 
       if(contador == 5){
         continue;
+        
       }
       
-      System.out.println(contador);
-      System.out.println("Eu nunca mais...");
+      System.out.println( contador );
+      System.out.println( "Eu nunca mais..." );
 
     }
 
-       System.out.println("Vida que segue");  
+      System.out.println( "Vida que segue" );  
+    
   }
 }
