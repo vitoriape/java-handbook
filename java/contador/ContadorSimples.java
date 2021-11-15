76ecf536bb580c92390c71b70d78f1c6a6467b3e
Vitória Peçanha <vitoriapecanha.log@gmail.com>
2021-11-15 14:50:36 -0300

    Update and rename Retorna11.java to ContadorSimples.java

---
+++ java/contador/ContadorSimples.java
@@ -0,0 +1,11 @@
+class ContadorSimples {
+  public static void main(String[] args) {
+    int contador = 0;  
+      while (contador <= 10){
+        System.out.println("Eu nunca mais...");
+
+        contador++;
+      }
+  }
+}
+
