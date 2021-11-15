366d47b8bdb16faaae525247c0cedab7f318f939
Vitória Peçanha <vitoriapecanha.log@gmail.com>
2021-11-15 14:55:17 -0300

    Update ComparacaoIfElse.java

--- java/operações/ComparacaoIfElse.java
+++ java/operações/ComparacaoIfElse.java
@@ -1,20 +1,21 @@
-public class ComparacaoIfElse {
-    public static void main(String args[]) {
-        double nota1 = 4.5;
-        double nota2 = 1.5;
+class ComparacaoIfElse {
+  public static void main(String[] args) {
+
+    double nota1 = 4.5;
+    double nota2 = 1.5;
         
-        double media = (nota1 + nota2) / 2;
+    double media = (nota1 + nota2) / 2;
         
-       if(media >= 5){
-           System.out.println("Passou !!!");
-       }
-       else if(media >= 3 && media < 5){
-           System.out.println("Exame");
-       }
-       else{ 
-           System.out.println("Não passou");
-       }
+    if(media >= 5){
+      System.out.println("Passou !!!");
+       
+    }else if(media >= 3 && media < 5){
+      System.out.println("Exame");
+
+    }else{ 
+      System.out.println("Não passou");
 
     }
+  }
 }
 
