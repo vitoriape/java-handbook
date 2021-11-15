d25ff56c3057a9d6e40423f20afe26618b319077
Vitória Peçanha <vitoriapecanha.log@gmail.com>
2021-11-15 14:53:35 -0300

    Update ComparacaoBoolean.java

--- java/operações/ComparacaoBoolean.java
+++ java/operações/ComparacaoBoolean.java
@@ -1,12 +1,14 @@
-public class ComparacaoBoolean {
-public static void main(String args[]) {
-        double nota1 = 8.5;
-        double nota2 = 9.4;
-        
-        double media = (nota1 + nota2) / 2;
-        
-        boolean passou = media >= 5;
+class ComparacaoBoolean {
+  public static void main(String[] args) {
 
-      System.out.println(passou);
-    }
+    double nota1 = 8.5;
+    double nota2 = 9.4;
+        
+    double media = (nota1 + nota2) / 2;
+        
+    boolean passou = media >= 5;
+
+    System.out.println(passou);
+
+  }
 }
