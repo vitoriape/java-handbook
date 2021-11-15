4d4387da3e6d8afd676deae61b153f8be6c4ec88
Vitória Peçanha <vitoriapecanha.log@gmail.com>
2021-11-15 14:46:10 -0300

    Create ContadorWhileInvertido.java

---
+++ java/contador/ContadorWhileInvertido.java
@@ -0,0 +1,14 @@
+class ContadorWhileInvertido {
+  public static void main(String[] args) {
+    
+    int contador = 0;
+    
+    do{
+      System.out.println("Eu nunca mais...");
+
+      contador++;
+
+    }while (contador <= 10);
+    
+  }
+}
